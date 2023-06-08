d9d1b5d3afb339120b869264c471fd9122e8c34f
shiamhub <skshiam@gmail.com>
2023-06-09 00:20:42 +0600

    added jwt token verify

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access'});
+  }
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if(err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+
+    next();
+  })
+}
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.np7fjqr.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,6 +49,7 @@ async function run() {
     app.post('/jwt', async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+      // console.log({token});
       res.send({ token });
     })
 
@@ -43,8 +62,19 @@ async function run() {
       const result = await usersCollection.find({}).toArray();
       res.send(result);
     })
-    app.get('/addCart', async (req, res) => {
-      const result = await cartCollection.find({}).toArray();
+
+    app.get('/addCart', verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+
+      if(req.decoded.email !== email) {
+        res.send({admin: false})
+      }
+      
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
       res.send(result);
     })
 
